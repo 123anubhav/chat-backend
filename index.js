90c476f610bf175a9dc9c68f73468c97be1a0b3c
Anubhav Chaudhary <31804245+123anubhav@users.noreply.github.com>
2024-07-21 10:08:18 +0530

    upload code

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
